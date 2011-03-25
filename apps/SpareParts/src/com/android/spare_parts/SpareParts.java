5eba3045b22064392867872262d9cf166fc5d3a2
ben <bendbowden@gmail.com>
2011-03-25 15:21:41 -0400

    Added several options for 4tab and rotary revamped. Also did a little more cleanup to improve appearance

--- apps/SpareParts/src/com/android/spare_parts/SpareParts.java
+++ apps/SpareParts/src/com/android/spare_parts/SpareParts.java
@@ -17,6 +17,7 @@
 
 package com.android.spare_parts;
 
+import android.content.Intent.ShortcutIconResource;
 import android.app.ActivityManagerNative;
 import android.content.Context;
 import android.content.Intent;
@@ -41,6 +42,8 @@ import android.util.Log;
 import android.view.IWindowManager;
 
 import java.util.List;
+import java.util.ArrayList;
+import com.android.spare_parts.R;
 
 public class SpareParts extends PreferenceActivity
         implements Preference.OnPreferenceChangeListener,
@@ -51,9 +54,20 @@ public class SpareParts extends PreferenceActivity
     private static final String BATTERY_INFORMATION_PREF = "battery_information_settings";
     private static final String USAGE_STATISTICS_PREF = "usage_statistics_settings";
     
-    //extra
-    private static final String USE_ROTARY_LOCKSCREEN_PREF = "use_rotary_lockscreen";
+    //lockscreen
+    private static final String LOCKSCREEN_STYLE_PREF = "pref_lockscreen_style";
+
+    private static final String LOCKSCREEN_CUSTOM_APP_TOGGLE = "pref_lockscreen_custom_app_toggle";
+
+    private static final String LOCKSCREEN_CUSTOM_APP_ACTIVITY = "pref_lockscreen_custom_app_activity";
+
+    private static final String LOCKSCREEN_ROTARY_UNLOCK_DOWN_TOGGLE = "pref_lockscreen_rotary_unlock_down_toggle";
+
+    private static final String LOCKSCREEN_ROTARY_HIDE_ARROWS_TOGGLE = "pref_lockscreen_rotary_hide_arrows_toggle";
+
     private static final String MENU_UNLOCK_SCREEN_PREF = "menu_unlock_screen";
+    //end lockscreen
+
     private static final String LAUNCHER_ORIENTATION_PREF = "launcher_orientation";
     private static final String DISPLAY_CLOCK_PREF = "display_clock";
     private static final String CLOCK_COLOR_PREF = "clock_color";
@@ -67,13 +81,17 @@ public class SpareParts extends PreferenceActivity
     private static final String KEY_COMPATIBILITY_MODE = "compatibility_mode";
 
     //extra
-    private CheckBoxPreference mUseRotaryLockscreenPref;
+    private ListPreference mLockscreenStylePref;
+    private CheckBoxPreference mCustomAppTogglePref;
+    private CheckBoxPreference mRotaryUnlockDownToggle;
+    private CheckBoxPreference mRotaryHideArrowsToggle;
     private CheckBoxPreference mMenuUnlockScreenPref;
     private CheckBoxPreference mLauncherOrientationPref;
     private CheckBoxPreference mDisplayClockPref;
     private Preference mClockColorPref;
     private CheckBoxPreference mBatteryPercentagePref;
     private Preference mBatteryColorPref;
+    private Preference mCustomAppActivityPref;
     //end extra
 
     private ListPreference mWindowAnimationsPref;
@@ -82,6 +100,14 @@ public class SpareParts extends PreferenceActivity
     private CheckBoxPreference mCompatibilityMode;
 
     private IWindowManager mWindowManager;
+
+    private int mKeyNumber = 1;
+
+    private static final int REQUEST_PICK_SHORTCUT = 1;
+
+    private static final int REQUEST_PICK_APPLICATION = 2;
+
+    private static final int REQUEST_CREATE_SHORTCUT = 3;
 
     public static boolean updatePreferenceToSpecificActivityOrRemove(Context context,
             PreferenceGroup parentPreferenceGroup, String preferenceKey, int flags) {
@@ -125,8 +151,17 @@ public class SpareParts extends PreferenceActivity
 
         PreferenceScreen prefSet = getPreferenceScreen();
         
-        //extra
-        mUseRotaryLockscreenPref = (CheckBoxPreference) prefSet.findPreference(USE_ROTARY_LOCKSCREEN_PREF);
+        //lockscreen style
+        mLockscreenStylePref = (ListPreference) prefSet.findPreference(LOCKSCREEN_STYLE_PREF);
+        int lockscreenStyle = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_STYLE_PREF, 1);
+        mLockscreenStylePref.setValue(String.valueOf(lockscreenStyle));
+        mLockscreenStylePref.setOnPreferenceChangeListener(this);
+	//phone & messaging tabs
+        mCustomAppTogglePref = (CheckBoxPreference) prefSet.findPreference(LOCKSCREEN_CUSTOM_APP_TOGGLE);
+        mRotaryUnlockDownToggle = (CheckBoxPreference) prefSet.findPreference(LOCKSCREEN_ROTARY_UNLOCK_DOWN_TOGGLE);
+         mRotaryHideArrowsToggle = (CheckBoxPreference) prefSet.findPreference(LOCKSCREEN_ROTARY_HIDE_ARROWS_TOGGLE);
+        mCustomAppActivityPref = (Preference) prefSet.findPreference(LOCKSCREEN_CUSTOM_APP_ACTIVITY);
         mMenuUnlockScreenPref = (CheckBoxPreference) prefSet.findPreference(MENU_UNLOCK_SCREEN_PREF);
         mLauncherOrientationPref = (CheckBoxPreference) prefSet.findPreference(LAUNCHER_ORIENTATION_PREF);
         mDisplayClockPref = (CheckBoxPreference) prefSet.findPreference(DISPLAY_CLOCK_PREF);
@@ -160,9 +195,15 @@ public class SpareParts extends PreferenceActivity
 
     private void updateToggles() {
         //extra
-        mUseRotaryLockscreenPref.setChecked(Settings.System.getInt(
+        mCustomAppTogglePref.setChecked(Settings.System.getInt(
                 getContentResolver(),
-                Settings.System.USE_ROTARY_LOCKSCREEN, 0) != 0);
+                Settings.System.LOCKSCREEN_CUSTOM_APP_TOGGLE, 0) != 0);
+        mRotaryUnlockDownToggle.setChecked(Settings.System.getInt(
+                getContentResolver(),
+                Settings.System.LOCKSCREEN_ROTARY_UNLOCK_DOWN, 0) != 0);
+        mRotaryHideArrowsToggle.setChecked(Settings.System.getInt(
+                getContentResolver(),
+                Settings.System.LOCKSCREEN_ROTARY_HIDE_ARROWS, 0) != 0);
         mMenuUnlockScreenPref.setChecked(Settings.System.getInt(
                 getContentResolver(), 
                 Settings.System.MENU_UNLOCK_SCREEN, 0) != 0);
@@ -181,11 +222,15 @@ public class SpareParts extends PreferenceActivity
                 Settings.System.FANCY_IME_ANIMATIONS, 0) != 0);
     }
     
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mWindowAnimationsPref) {
-            writeAnimationPreference(0, objValue);
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenStylePref) {
+            int lockscreenStyle = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_STYLE_PREF,
+                    lockscreenStyle);
+        } else if (preference == mWindowAnimationsPref) {
+            writeAnimationPreference(0, newValue);
         } else if (preference == mTransitionAnimationsPref) {
-            writeAnimationPreference(1, objValue);
+            writeAnimationPreference(1, newValue);
         }
         // always let the preference setting proceed.
         return true;
@@ -212,8 +257,12 @@ public class SpareParts extends PreferenceActivity
             readBatteryFontColor());
             cp.show();            
         }
+        else if (preference == mCustomAppActivityPref) {
+            pickShortcut(4);
+	}
         //end extra
         return false;
+
     }
 
     public void writeAnimationPreference(int which, Object objValue) {
@@ -246,8 +295,77 @@ public class SpareParts extends PreferenceActivity
         } catch (RemoteException e) {
         }
     }
+    //custom app picker
+    private void pickShortcut(int keyNumber) {
+        mKeyNumber = keyNumber;
+        Bundle bundle = new Bundle();
+        ArrayList<String> shortcutNames = new ArrayList<String>();
+        shortcutNames.add(getString(R.string.group_applications));
+        bundle.putStringArrayList(Intent.EXTRA_SHORTCUT_NAME, shortcutNames);
+        ArrayList<ShortcutIconResource> shortcutIcons = new ArrayList<ShortcutIconResource>();
+        shortcutIcons.add(ShortcutIconResource
+                .fromContext(this, R.drawable.ic_launcher_application));
+        bundle.putParcelableArrayList(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, shortcutIcons);
+        Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+        pickIntent.putExtra(Intent.EXTRA_INTENT, new Intent(Intent.ACTION_CREATE_SHORTCUT));
+        pickIntent.putExtra(Intent.EXTRA_TITLE, getText(R.string.select_custom_app_title));
+        pickIntent.putExtras(bundle);
+        startActivityForResult(pickIntent, REQUEST_PICK_SHORTCUT);
+    }
 
-    //extra
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_PICK_APPLICATION:
+                    completeSetCustomApp(data);
+                    break;
+                case REQUEST_CREATE_SHORTCUT:
+                    completeSetCustomShortcut(data);
+                    break;
+                case REQUEST_PICK_SHORTCUT:
+                    processShortcut(data, REQUEST_PICK_APPLICATION, REQUEST_CREATE_SHORTCUT);
+                    break;
+            }
+        }
+    }
+
+    void processShortcut(Intent intent, int requestCodeApplication, int requestCodeShortcut) {
+        // Handle case where user selected "Applications"
+        String applicationName = getResources().getString(R.string.group_applications);
+        String shortcutName = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+        if (applicationName != null && applicationName.equals(shortcutName)) {
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+            Intent pickIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+            pickIntent.putExtra(Intent.EXTRA_INTENT, mainIntent);
+            startActivityForResult(pickIntent, requestCodeApplication);
+        } else {
+            startActivityForResult(intent, requestCodeShortcut);
+        }
+    }
+
+    void completeSetCustomShortcut(Intent data) {
+        Intent intent = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_INTENT);
+        int keyNumber = mKeyNumber;
+        if (keyNumber == 4) {
+            if (Settings.System.putString(getContentResolver(),
+                    Settings.System.LOCKSCREEN_CUSTOM_APP_ACTIVITY, intent.toUri(0))) {
+                mCustomAppActivityPref.setSummary(intent.toUri(0));
+            }
+        }
+    }
+
+    void completeSetCustomApp(Intent data) {
+        int keyNumber = mKeyNumber;
+        if (keyNumber == 4) {
+            if (Settings.System.putString(getContentResolver(),
+                    Settings.System.LOCKSCREEN_CUSTOM_APP_ACTIVITY, data.toUri(0))) {
+                mCustomAppActivityPref.setSummary(data.toUri(0));
+            }
+        }
+    }
+    //end custom app picker
     private int readClockFontColor() {
         try {
             return Settings.System.getInt(getContentResolver(), Settings.System.CLOCK_COLOR);
@@ -280,14 +398,22 @@ public class SpareParts extends PreferenceActivity
 
     public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
         //extra
-        if (USE_ROTARY_LOCKSCREEN_PREF.equals(key)) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.USE_ROTARY_LOCKSCREEN,
-                    mUseRotaryLockscreenPref.isChecked() ? 1 : 0);
-        } else if (MENU_UNLOCK_SCREEN_PREF.equals(key)) {
+        if (MENU_UNLOCK_SCREEN_PREF.equals(key)) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.MENU_UNLOCK_SCREEN,
                     mMenuUnlockScreenPref.isChecked() ? 1 : 0);
+        } else if (LOCKSCREEN_CUSTOM_APP_TOGGLE.equals(key)) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_CUSTOM_APP_TOGGLE,
+                    mCustomAppTogglePref.isChecked() ? 1 : 0);
+        } else if (LOCKSCREEN_ROTARY_HIDE_ARROWS_TOGGLE.equals(key)) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_ROTARY_HIDE_ARROWS,
+                    mRotaryHideArrowsToggle.isChecked() ? 1 : 0);
+        } else if (LOCKSCREEN_ROTARY_UNLOCK_DOWN_TOGGLE.equals(key)) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_ROTARY_UNLOCK_DOWN,
+                    mRotaryUnlockDownToggle.isChecked() ? 1 : 0);
         } else if (LAUNCHER_ORIENTATION_PREF.equals(key)) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.LAUNCHER_ORIENTATION,
@@ -314,5 +440,17 @@ public class SpareParts extends PreferenceActivity
         readAnimationPreference(0, mWindowAnimationsPref);
         readAnimationPreference(1, mTransitionAnimationsPref);
         updateToggles();
+        mCustomAppActivityPref.setSummary(Settings.System.getString(getContentResolver(),
+                Settings.System.LOCKSCREEN_CUSTOM_APP_ACTIVITY));
+    }
+
+    private boolean isDefaultLockscreenStyle() {
+        int lockscreenStyle = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_STYLE_PREF, 1);
+        if (lockscreenStyle == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
